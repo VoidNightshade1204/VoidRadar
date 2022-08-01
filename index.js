c04f150d3643180d8c33b65792b3e644237eb1bb
SteepAtticStairs <lawlorcube06@gmail.com>
2022-07-31 23:18:09 -0400

    start of mesocyclone data

--- index.js
+++ index.js
@@ -279,9 +279,45 @@ document.addEventListener('loadFile', function(event) {
 
                 document.getElementById('radDate').innerHTML = finalRadarDateTime;
 
-                if (l3rad.textHeader.type == "NST") {
-                var stormTracksLayerArr = [];
-                // load storm tracking information
+                if (l3rad.textHeader.type == "NMD") {
+                    var mesocycloneLayersArr = [];
+                    var geojsonPointTemplate = {
+                        'type': 'Feature',
+                        'properties': {},
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': 'he'
+                        }
+                    }
+                    $.getJSON('https://steepatticstairs.github.io/weather/json/radarStations.json', function(data) {
+                        var staLat = data[theFileStation][1];
+                        var staLng = data[theFileStation][2];
+
+                        var mesocycloneObj = l3rad.formatted.mesocyclone;
+                        console.log(mesocycloneObj)
+                        var mesocycloneList = Object.keys(mesocycloneObj);
+
+                        function loadMesocyclone(identifier) {
+                            // store all map layers being added to be able to manipulate later
+                            mesocycloneLayersArr.push(identifier)
+                            // reset geojson coordinates
+                            geojsonPointTemplate.geometry.coordinates = [];
+
+                            // current storm track
+                            var curMC = mesocycloneObj[identifier];
+                            var curMCCoords = findTerminalCoordinates(staLat, staLng, curMC.az, curMC.ran);
+                            // push the initial coordinate point - we do not know if the current track is a line or a point yet
+                            geojsonPointTemplate.geometry.coordinates = [curMCCoords.longitude, curMCCoords.latitude];
+
+                            setGeojsonLayer(geojsonPointTemplate, 'circle', identifier)
+                        }
+                        for (key in mesocycloneList) {
+                            loadMesocyclone(mesocycloneList[key])
+                        }
+                    });
+                } else if (l3rad.textHeader.type == "NST") {
+                    var stormTracksLayerArr = [];
+                    // load storm tracking information
                     var geojsonLineTemplate = {
                         'type': 'Feature',
                         'properties': {},
