4ec120522fddc7d7ccefeceed6b0737e48797683
SteepAtticStairs <lawlorcube06@gmail.com>
2022-08-03 00:39:47 -0400

    only one loading for the l2 file

--- dist/bundle.js
+++ dist/bundle.js
@@ -37297,32 +37297,50 @@ document.addEventListener('loadFile', function(event) {
                 //$('.reflPlotButton').trigger('click');
                 //console.log('initial reflectivity plot');
                 //displayElevations('REF');
-                if (event.detail[3] == undefined || event.detail[3] == 'l2-ref') {
-                    const level2Plot = plot(l2rad, 'REF', {
-                        elevations: 1,
-                    });
-                } else if (event.detail[3] == 'l2-vel') {
-                    const level2Plot = plot(l2rad, 'VEL', {
-                        elevations: 2,
-                    });
-                } else if (event.detail[3] == 'l2-rho') {
-                    const level2Plot = plot(l2rad, 'RHO', {
-                        elevations: 1,
-                    });
-                } else if (event.detail[3] == 'l2-phi') {
-                    const level2Plot = plot(l2rad, 'PHI', {
-                        elevations: 1,
-                    });
-                } else if (event.detail[3] == 'l2-zdr') {
-                    const level2Plot = plot(l2rad, 'ZDR', {
-                        elevations: 1,
-                    });
-                } else if (event.detail[3] == 'l2-sw ') {
-                    displayElevations('SW ');
-                    const level2Plot = plot(l2rad, 'SW ', {
-                        elevations: parseInt($('#elevInput').val()),
-                    });
+                var btnsArr = [
+                    "l2-ref",
+                    "l2-vel",
+                    "l2-rho",
+                    "l2-phi",
+                    "l2-zdr",
+                    "l2-sw "
+                ]
+                for (key in btnsArr) {
+                    var curElemIter = document.getElementById(btnsArr[key]);
+                    curElemIter.disabled = false;
+                    $(curElemIter).addClass('btn-outline-primary');
+                    $(curElemIter).removeClass('btn-outline-secondary');
                 }
+                document.getElementById('loadl2').style.display = 'none';
+                $('.productBtnGroup button').on('click', function() {
+                    removeMapLayer('baseReflectivity');
+                    if (this.value == 'l2-ref') {
+                        const level2Plot = plot(l2rad, 'REF', {
+                            elevations: 1,
+                        });
+                    } else if (this.value == 'l2-vel') {
+                        const level2Plot = plot(l2rad, 'VEL', {
+                            elevations: 2,
+                        });
+                    } else if (this.value == 'l2-rho') {
+                        const level2Plot = plot(l2rad, 'RHO', {
+                            elevations: 1,
+                        });
+                    } else if (this.value == 'l2-phi') {
+                        const level2Plot = plot(l2rad, 'PHI', {
+                            elevations: 1,
+                        });
+                    } else if (this.value == 'l2-zdr') {
+                        const level2Plot = plot(l2rad, 'ZDR', {
+                            elevations: 1,
+                        });
+                    } else if (this.value == 'l2-sw ') {
+                        displayElevations('SW ');
+                        const level2Plot = plot(l2rad, 'SW ', {
+                            elevations: parseInt($('#elevInput').val()),
+                        });
+                    }
+                });
                 $('#productInput').on('change', function() {
                     removeMapLayer('baseReflectivity');
                     if ($('#productInput').val() == 'REF') {
