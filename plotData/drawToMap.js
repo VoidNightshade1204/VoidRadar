8ef31f5dbb3e823d57f3a64a085187366711637b
SteepAtticStairs <lawlorcube06@gmail.com>
2022-08-03 23:11:31 -0400

    more efficient divider code

--- plotData/drawToMap.js
+++ plotData/drawToMap.js
@@ -8,12 +8,13 @@ function drawRadarShape(jsonObj, lati, lngi, produc, shouldFilter) {
     settings["phi"] = 0.483395;
     settings["base"] = jsonObj;
 
+    if (Array.isArray(produc)) {
+        produc = produc[0];
+    }
+
 
     var divider;
     function createTexture(gl) {
-        if (Array.isArray(produc)) {
-            produc = produc[0];
-        }
         $.getJSON(`./plotData/products/${produc}.json`, function(data) {
             console.log(data);
             var colors = data.colors; //colors["ref"];
@@ -105,7 +106,7 @@ function drawRadarShape(jsonObj, lati, lngi, produc, shouldFilter) {
 
     //var myWorker = new Worker('./polygonTest/generateVerticesRadarDemo.js');
     //myWorker.onmessage = function (oEvent) {
-    function finishItUp(data, indices, colors, geojson) {
+    function finishItUp(data, indices, colors, layer, geojson) {
         //var data = new Float32Array(oEvent.data.data);
         //var indices = new Int32Array(oEvent.data.indices);
         //var colors = new Float32Array(oEvent.data.colors);
@@ -191,159 +192,133 @@ function drawRadarShape(jsonObj, lati, lngi, produc, shouldFilter) {
         document.getElementById('testEventElem').innerHTML = 'changed'
     }
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var vers = JSON.parse(this.responseText).version;
+    $.getJSON(`./plotData/products/${produc}.json`, function(data) {
+        divider = data.divider;
+    }).then(function() {
+        console.log('NEW!!!!')
+        console.log(divider)
+        //compile shaders
+        var vertexSource = `
+            //x: azimuth
+            //y: range
+            //z: value
+            attribute vec2 aPosition;
+            attribute float aColor;
+            uniform mat4 u_matrix;
+            varying float color;
+    
+            void main() {
+                color = aColor;
+                gl_Position = u_matrix * vec4(aPosition.x,aPosition.y,0.0,1.0);
+            }`;
+        var fragmentSource = `
+            precision mediump float;
+            varying float color;
+            uniform sampler2D u_texture;
+            void main() {
+                //gl_FragColor = vec4(0.0,color/60.0,0.0,1.0);
+                float calcolor = (color)${divider};
+                gl_FragColor = texture2D(u_texture,vec2(min(max(calcolor,0.0),1.0),0.0));
+                //gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+            }`
+        var masterGl;
+        var layer = {
+            id: "baseReflectivity",
+            type: "custom",
+            minzoom: 0,
+            maxzoom: 18,
 
-            calcPolys.calcPolygons(
-                settings["base"],
-                settings["phi"],
-                settings["rlat"],
-                settings["rlon"],
-                vers,
-                function(dat) {
-                    finishItUp(dat.data, dat.indices, dat.colors)
-                }
-            )
-            //myWorker.postMessage([
-            //    settings["base"],
-            //    settings["phi"],
-            //    settings["rlat"],
-            //    settings["rlon"],
-            //    vers
-            //]);
+            onAdd: function (map, gl) {
+                masterGl = gl;
+                createTexture(gl);
+
+                var ext = gl.getExtension('OES_element_index_uint');
+                var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+                gl.shaderSource(vertexShader, vertexSource);
+                gl.compileShader(vertexShader);
+                var compilationLog = gl.getShaderInfoLog(vertexShader);
+                var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+                gl.shaderSource(fragmentShader, fragmentSource);
+                gl.compileShader(fragmentShader);
+                var compilationLog = gl.getShaderInfoLog(fragmentShader);
+                this.program = gl.createProgram();
+                gl.attachShader(this.program, vertexShader);
+                gl.attachShader(this.program, fragmentShader);
+                gl.linkProgram(this.program);
+                this.matrixLocation = gl.getUniformLocation(this.program, "u_matrix");
+                this.positionLocation = gl.getAttribLocation(this.program, "aPosition");
+                this.colorLocation = gl.getAttribLocation(this.program, "aColor");
+                this.textureLocation = gl.getUniformLocation(this.program, "u_texture");
+
+                //data buffers
+                this.positionBuffer = gl.createBuffer();
+                this.indexBuffer = gl.createBuffer();
+                this.colorBuffer = gl.createBuffer();
+            },//end onAdd
+            render: function (gl, matrix) {
+                //console.log("render base");
+                var ext = gl.getExtension('OES_element_index_uint');
+                //use program
+                gl.useProgram(this.program);
+                //how to remove vertices from position buffer
+                var size = 2;
+                var type = gl.FLOAT;
+                var normalize = false;
+                var stride = 0;
+                var offset = 0;
+                //calculate matrices
+                gl.uniformMatrix4fv(this.matrixLocation, false, matrix);
+                gl.uniform1i(this.textureLocation, 0);
+                gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
+                gl.bufferData(gl.ARRAY_BUFFER, pageState.positions, gl.STATIC_DRAW);
+                gl.enableVertexAttribArray(this.positionLocation);
+                gl.vertexAttribPointer(this.positionLocation, size, type, normalize, stride, offset);
+
+                gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
+                gl.bufferData(gl.ARRAY_BUFFER, pageState.colors, gl.STATIC_DRAW);
+                gl.enableVertexAttribArray(this.colorLocation);
+                gl.vertexAttribPointer(this.colorLocation, 1, type, normalize, stride, offset);
+
+                gl.bindTexture(gl.TEXTURE_2D, pageState.imagetexture);
+                gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, imagedata)
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+                var primitiveType = gl.TRIANGLES;
+                var count = pageState.indices.length;
+                gl.drawArrays(primitiveType, offset, pageState.positions.length / 2);
+
+            }//end render
         }
-    };
-    xhttp.open("GET", jsonObj, true);
-    xhttp.send();
 
-    if (produc == "REF" || produc[0] == "N0B") {
-        divider = '/(70.0)';
-    } else if (produc == "VEL") {
-        divider = '/(70.0)';
-    } else if (produc == "RHO") {
-        divider = '';
-    } else if (produc == "PHI") {
-        divider = '/(135.0)';
-    } else if (produc == "ZDR") {
-        divider = '/(20.0)';
-    } else if (produc == "SW " || produc[0] == "NSW") {
-        divider = '/(10.0)';
-    }
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                var vers = JSON.parse(this.responseText).version;
 
-    else if (produc == "HHC" || produc[0] == "N0H") {
-        divider = '/(155.0)';
-    } else if (produc[0] == "N0S") {
-        divider = '/(16.0)';
-    } else if (produc[0] == "NXQ") {
-        divider = '/(70.0)';
-    } else if (produc[0] == "N0U") {
-        divider = '/(135.0)';
-    } else if (produc[0] == "N0G") {
-        divider = '/(135.0)';
-    } else if (produc[0] == "DVL") {
-        divider = '/(90.0)';
-    } else if (produc[0] == "N0C") {
-        divider = '/(255.0)';
-    } else if (produc[0] == "N0X") {
-        divider = '/(255.0)';
-    }
-    console.log(divider)
-    //compile shaders
-    var vertexSource = `
-        //x: azimuth
-        //y: range
-        //z: value
-        attribute vec2 aPosition;
-        attribute float aColor;
-        uniform mat4 u_matrix;
-        varying float color;
-
-        void main() {
-            color = aColor;
-            gl_Position = u_matrix * vec4(aPosition.x,aPosition.y,0.0,1.0);
-        }`;
-    var fragmentSource = `
-        precision mediump float;
-        varying float color;
-        uniform sampler2D u_texture;
-        void main() {
-            //gl_FragColor = vec4(0.0,color/60.0,0.0,1.0);
-            float calcolor = (color)${divider};
-            gl_FragColor = texture2D(u_texture,vec2(min(max(calcolor,0.0),1.0),0.0));
-            //gl_FragColor = vec4(1.0,0.0,0.0,1.0);
-        }`
-    var masterGl;
-    var layer = {
-        id: "baseReflectivity",
-        type: "custom",
-        minzoom: 0,
-        maxzoom: 18,
-
-        onAdd: function (map, gl) {
-            masterGl = gl;
-            createTexture(gl);
-
-            var ext = gl.getExtension('OES_element_index_uint');
-            var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-            gl.shaderSource(vertexShader, vertexSource);
-            gl.compileShader(vertexShader);
-            var compilationLog = gl.getShaderInfoLog(vertexShader);
-            var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-            gl.shaderSource(fragmentShader, fragmentSource);
-            gl.compileShader(fragmentShader);
-            var compilationLog = gl.getShaderInfoLog(fragmentShader);
-            this.program = gl.createProgram();
-            gl.attachShader(this.program, vertexShader);
-            gl.attachShader(this.program, fragmentShader);
-            gl.linkProgram(this.program);
-            this.matrixLocation = gl.getUniformLocation(this.program, "u_matrix");
-            this.positionLocation = gl.getAttribLocation(this.program, "aPosition");
-            this.colorLocation = gl.getAttribLocation(this.program, "aColor");
-            this.textureLocation = gl.getUniformLocation(this.program, "u_texture");
-
-            //data buffers
-            this.positionBuffer = gl.createBuffer();
-            this.indexBuffer = gl.createBuffer();
-            this.colorBuffer = gl.createBuffer();
-        },//end onAdd
-        render: function (gl, matrix) {
-            //console.log("render base");
-            var ext = gl.getExtension('OES_element_index_uint');
-            //use program
-            gl.useProgram(this.program);
-            //how to remove vertices from position buffer
-            var size = 2;
-            var type = gl.FLOAT;
-            var normalize = false;
-            var stride = 0;
-            var offset = 0;
-            //calculate matrices
-            gl.uniformMatrix4fv(this.matrixLocation, false, matrix);
-            gl.uniform1i(this.textureLocation, 0);
-            gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
-            gl.bufferData(gl.ARRAY_BUFFER, pageState.positions, gl.STATIC_DRAW);
-            gl.enableVertexAttribArray(this.positionLocation);
-            gl.vertexAttribPointer(this.positionLocation, size, type, normalize, stride, offset);
-
-            gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
-            gl.bufferData(gl.ARRAY_BUFFER, pageState.colors, gl.STATIC_DRAW);
-            gl.enableVertexAttribArray(this.colorLocation);
-            gl.vertexAttribPointer(this.colorLocation, 1, type, normalize, stride, offset);
-
-            gl.bindTexture(gl.TEXTURE_2D, pageState.imagetexture);
-            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, imagedata)
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-
-            var primitiveType = gl.TRIANGLES;
-            var count = pageState.indices.length;
-            gl.drawArrays(primitiveType, offset, pageState.positions.length / 2);
-
-        }//end render
-    }
+                calcPolys.calcPolygons(
+                    settings["base"],
+                    settings["phi"],
+                    settings["rlat"],
+                    settings["rlon"],
+                    vers,
+                    function(dat) {
+                        finishItUp(dat.data, dat.indices, dat.colors, layer)
+                    }
+                )
+                //myWorker.postMessage([
+                //    settings["base"],
+                //    settings["phi"],
+                //    settings["rlat"],
+                //    settings["rlon"],
+                //    vers
+                //]);
+            }
+        };
+        xhttp.open("GET", jsonObj, true);
+        xhttp.send();
+    })
 }
 
 module.exports = {
