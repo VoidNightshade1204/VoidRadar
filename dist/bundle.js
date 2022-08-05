6f7618920ec24d8f12e1a692dbce66572a509435
SteepAtticStairs <lawlorcube06@gmail.com>
2022-08-04 23:46:50 -0400

    added visibility control for the radar

--- dist/bundle.js
+++ dist/bundle.js
@@ -37278,6 +37278,8 @@ const STstuff = require('./stormTracking');
 const tt = require('./paletteTooltip');
 var map = require('./map/map');
 
+var hasVisibilityControl = false;
+
 function drawRadarShape(jsonObj, lati, lngi, produc, shouldFilter) {
     var settings = {};
     settings["rlat"] = lati;
@@ -37350,6 +37352,53 @@ function drawRadarShape(jsonObj, lati, lngi, produc, shouldFilter) {
         pageState.colors = colors;
         //console.log(Math.max(...[...new Set(colors)]))
         map.addLayer(layer);
+
+        class visibilityControl {
+            onAdd(map) {
+                this._map = map;
+                this._container = document.createElement('div');
+                this._container.innerHTML = `
+                    <div class="mapboxgl-control-container" style="margin-top: 100%;">
+                        <div class="mapboxgl-ctrl mapboxgl-ctrl-group">
+                            <button class="mapboxgl-ctrl-fullscreen" type="button" aria-label="Globe Toggle">
+                                <span class="fa fa-eye icon-black" id="visibilityThing" aria-hidden="true" title="Globe Toggle"></span>
+                            </button>
+                        </div>
+                    </div>`
+                this._container.addEventListener('click', function () {
+                    if (!$('#visibilityThing').hasClass('icon-selected')) {
+                        $('#visibilityThing').addClass('icon-selected');
+
+                        $('#visibilityThing').removeClass('fa-eye');
+                        $('#visibilityThing').addClass('fa-eye-slash');
+
+                        $('#visibilityThing').removeClass('icon-black');
+                        map.setLayoutProperty('baseReflectivity', 'visibility', 'none');
+                    } else if ($('#visibilityThing').hasClass('icon-selected')) {
+                        $('#visibilityThing').removeClass('icon-selected');
+
+                        $('#visibilityThing').removeClass('fa-eye-slash');
+                        $('#visibilityThing').addClass('fa-eye');
+
+                        
+                        $('#visibilityThing').addClass('icon-black');
+                        map.setLayoutProperty('baseReflectivity', 'visibility', 'visible');
+                    }
+                })
+                return this._container;
+            }
+
+            onRemove() {
+                this._container.parentNode.removeChild(this._container);
+                this._map = undefined;
+            }
+        }
+        var theVisibilityControl = new visibilityControl;
+        if (!hasVisibilityControl) {
+            map.addControl(theVisibilityControl, 'top-left');
+            hasVisibilityControl = true;
+        }
+        //map.addControl(theVisibilityControl, 'top-left');
 
         STstuff.loadAllStormTrackingStuff();
     }
