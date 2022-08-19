87375e38a6cd72db494f0a4be101af8ceeb22a60
SteepAtticStairs <lawlorcube06@gmail.com>
2022-08-19 14:25:06 -0400

    added level 2 / 3 file selector

--- app/radar/dom/fileUpload.js
+++ app/radar/dom/fileUpload.js
@@ -4,17 +4,12 @@ const ut = require('../utils');
 * https://stackoverflow.com/a/55576752
 */
 
-function handleFileSelect(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
-
-    var files = evt.dataTransfer.files;
-
+function dispatchEvent(filesObj) {
     // Create the event
     var event = new CustomEvent("loadFile", {
         "detail": [
-            files[0],
-            2,
+            filesObj[0],
+            parseInt($('#dataDiv').data('currentLevelInput')),
             'whole'
         ]
     });
@@ -23,6 +18,15 @@ function handleFileSelect(evt) {
 
     ut.disableModeBtn();
     $('#dataDiv').data('fromFileUpload', true);
+}
+
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    var files = evt.dataTransfer.files;
+
+    dispatchEvent(files);
 }
 
 function handleDragOver(evt) {
@@ -34,19 +38,8 @@ function handleDragOver(evt) {
 document.getElementById('hiddenFileUploader').addEventListener('input', function() {
     var files = document.getElementById('hiddenFileUploader').files;
     $('#dataDiv').data('fileName', files[0].name)
-    // Create the event
-    var event = new CustomEvent("loadFile", {
-        "detail": [
-            files[0],
-            2,
-            'whole'
-        ]
-    });
-    // Dispatch/Trigger/Fire the event
-    document.dispatchEvent(event);
 
-    ut.disableModeBtn();
-    $('#dataDiv').data('fromFileUpload', true);
+    dispatchEvent(files);
 })
 
 // Setup the dnd listeners.
@@ -55,22 +48,42 @@ dropZone.addEventListener('dragover', handleDragOver, false);
 dropZone.addEventListener('drop', handleFileSelect, false);
 
 
-function mouseEnter(thisObj) {
+function dragEnter(thisObj) {
     $(thisObj).animate({
         backgroundColor: 'rgb(212, 212, 212)',
+        'border-width': '5px',
+        'border-color': 'rgb(17, 167, 17)'
     }, 150);
 }
-function mouseLeave(thisObj) {
+function dragLeave(thisObj) {
     $(thisObj).animate({
         backgroundColor: 'white',
+        'border-width': '1px',
+        'border-color': 'rgb(72, 72, 72)'
     }, 150);
 }
 $('#drop_zone').on('mouseenter', function(e) {
-    mouseEnter(this);
+    $(this).animate({
+        backgroundColor: 'rgb(212, 212, 212)',
+    }, 150);
+})
+$('#drop_zone').on('dragenter', function(e) {
+    dragEnter(this);
 })
 $('#drop_zone').on('mouseleave', function(e) {
-    mouseLeave(this);
+    $(this).animate({
+        backgroundColor: 'white',
+    }, 150);
+})
+$('#drop_zone').on('dragleave', function(e) {
+    dragLeave(this);
 })
 $('#drop_zone').on('click', function(e) {
     $('#hiddenFileUploader').click();
+})
+
+$('#dataDiv').data('currentLevelInput', '2');
+$('.levelRadioInputs').on('click', function() {
+    $('#dataDiv').data('currentLevelInput', this.value);
+    document.getElementById('drop_zone').innerHTML = `Drop Level ${this.value} file here`;
 })
