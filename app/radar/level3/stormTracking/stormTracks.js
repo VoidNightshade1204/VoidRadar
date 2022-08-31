b299999f1f2c4b1b4a4061373c1d8988d61cfebd
SteepAtticStairs <lawlorcube06@gmail.com>
2022-08-31 11:48:29 -0400

    fixed level 3 file uploading

--- app/radar/level3/stormTracking/stormTracks.js
+++ app/radar/level3/stormTracking/stormTracks.js
@@ -84,8 +84,8 @@ function parsePlotStormTracks(l3rad, theFileStation) {
 
         var hourMin = ut.printHourMin(cellTime, ut.userTimeZone);
 
-        var popupHTML = 
-        `<div>Cell <b>${cellProperties.cellID}</b> at <b>${hourMin}</b></div>`
+        var popupHTML =
+            `<div>Cell <b>${cellProperties.cellID}</b> at <b>${hourMin}</b></div>`
 
         function flip(num) {
             if (num >= 180) {
@@ -105,106 +105,112 @@ function parsePlotStormTracks(l3rad, theFileStation) {
             .addTo(map);
     }
 
-    $.getJSON('https://steepatticstairs.github.io/NexradJS/resources/radarStations.json', function (data) {
-        var staLat = data[theFileStation][1];
-        var staLng = data[theFileStation][2];
 
-        var stormTracks = l3rad.formatted.storms;
-        //console.log(stormTracks)
-        var stormTracksList = Object.keys(stormTracks);
+    if (theFileStation.toUpperCase() != theFileStation) {
+        $.getJSON('https://steepatticstairs.github.io/NexradJS/resources/stationAbbreviations.json', function (abrvData) {
+            var theFileStation = abrvData[l3rad.textHeader.id3];
+            $.getJSON('https://steepatticstairs.github.io/NexradJS/resources/radarStations.json', function (data) {
+                var staLat = data[theFileStation][1];
+                var staLng = data[theFileStation][2];
 
-        var fileTime = getLevel3FileTime(l3rad);
+                var stormTracks = l3rad.formatted.storms;
+                //console.log(stormTracks)
+                var stormTracksList = Object.keys(stormTracks);
 
-        function loadStormTrack(identifier) {
-            // array to store all of the line points
-            var lineCoords = [];
+                var fileTime = getLevel3FileTime(l3rad);
 
-            // current storm track
-            var curST = stormTracks[identifier].current;
-            var curSTCoords = ut.findTerminalCoordinates(staLat, staLng, curST.nm, curST.deg);
-            // push the initial coordinate point - we do not know if the current track is a line or a point yet
-            var initialPoint = [curSTCoords.longitude, curSTCoords.latitude];
-            lineCoords.push(initialPoint)
+                function loadStormTrack(identifier) {
+                    // array to store all of the line points
+                    var lineCoords = [];
 
-            // current track's direction (degrees) and speed (knots)
-            // curSTMovement.deg, curSTMovement.kts
-            var curSTMovement = stormTracks[identifier].movement;
+                    // current storm track
+                    var curST = stormTracks[identifier].current;
+                    var curSTCoords = ut.findTerminalCoordinates(staLat, staLng, curST.nm, curST.deg);
+                    // push the initial coordinate point - we do not know if the current track is a line or a point yet
+                    var initialPoint = [curSTCoords.longitude, curSTCoords.latitude];
+                    lineCoords.push(initialPoint)
 
-            var curSTProperties = {
-                'movement': curSTMovement,
-                'cellID': identifier,
-                'coords': curSTCoords,
-                'time': fileTime.getTime()
-            }
+                    // current track's direction (degrees) and speed (knots)
+                    // curSTMovement.deg, curSTMovement.kts
+                    var curSTMovement = stormTracks[identifier].movement;
 
-            // future storm track (forecast)
-            var futureST = stormTracks[identifier].forecast;
-            var isLine;
-            // if the first forecast value for the current track is null, there is no line track - it is a point
-            if (futureST[0] == null) {
-                isLine = false;
-            } else if (futureST[0] != null) {
-                isLine = true;
-            }
-            if (isLine) {
-                for (key in futureST) {
-                    // the current index in the futureST variable being looped through
-                    var indexedFutureST = futureST[key];
-                    // check if the value is null, in which case the storm track is over
-                    if (indexedFutureST != null) {
-                        var indexedFutureSTCoords = ut.findTerminalCoordinates(staLat, staLng, indexedFutureST.nm, indexedFutureST.deg);
-                        var formattedFutureSTCoords = [indexedFutureSTCoords.longitude, indexedFutureSTCoords.latitude];
-                        // push the current index point to the line geojson
-                        lineCoords.push(formattedFutureSTCoords);
-                        // properties for the line circle
-                        var forecastSTProperties = {
-                            'movement': curSTMovement,
-                            'cellID': identifier,
-                            'coords': indexedFutureSTCoords,
-                            'index': key,
-                            'time': fileTime.getTime()
+                    var curSTProperties = {
+                        'movement': curSTMovement,
+                        'cellID': identifier,
+                        'coords': curSTCoords,
+                        'time': fileTime.getTime()
+                    }
+
+                    // future storm track (forecast)
+                    var futureST = stormTracks[identifier].forecast;
+                    var isLine;
+                    // if the first forecast value for the current track is null, there is no line track - it is a point
+                    if (futureST[0] == null) {
+                        isLine = false;
+                    } else if (futureST[0] != null) {
+                        isLine = true;
+                    }
+                    if (isLine) {
+                        for (key in futureST) {
+                            // the current index in the futureST variable being looped through
+                            var indexedFutureST = futureST[key];
+                            // check if the value is null, in which case the storm track is over
+                            if (indexedFutureST != null) {
+                                var indexedFutureSTCoords = ut.findTerminalCoordinates(staLat, staLng, indexedFutureST.nm, indexedFutureST.deg);
+                                var formattedFutureSTCoords = [indexedFutureSTCoords.longitude, indexedFutureSTCoords.latitude];
+                                // push the current index point to the line geojson
+                                lineCoords.push(formattedFutureSTCoords);
+                                // properties for the line circle
+                                var forecastSTProperties = {
+                                    'movement': curSTMovement,
+                                    'cellID': identifier,
+                                    'coords': indexedFutureSTCoords,
+                                    'index': key,
+                                    'time': fileTime.getTime()
+                                }
+                                // add a circle for each edge on a storm track line
+                                pushNewLinePoint(formattedFutureSTCoords, forecastSTProperties);
+                            }
                         }
-                        // add a circle for each edge on a storm track line
-                        pushNewLinePoint(formattedFutureSTCoords, forecastSTProperties);
+
+                        // add the finished line to the map
+                        pushNewMultiLineString(lineCoords)
+                        // adds a blue circle at the start of the storm track
+                        pushNewMainLinePoint(initialPoint, curSTProperties);
+                    } else if (!isLine) {
+                        // if the storm track does not have a forecast, display a Point geojson
+                        pushNewSinglePoint(initialPoint, curSTProperties);
                     }
                 }
 
-                // add the finished line to the map
-                pushNewMultiLineString(lineCoords)
-                // adds a blue circle at the start of the storm track
-                pushNewMainLinePoint(initialPoint, curSTProperties);
-            } else if (!isLine) {
-                // if the storm track does not have a forecast, display a Point geojson
-                pushNewSinglePoint(initialPoint, curSTProperties);
-            }
-        }
+                // Z0 = line, R1 = point
+                for (key in stormTracksList) {
+                    loadStormTrack(stormTracksList[key])
+                }
 
-        // Z0 = line, R1 = point
-        for (key in stormTracksList) {
-            loadStormTrack(stormTracksList[key])
-        }
+                // add the storm track lines
+                mapFuncs.setGeojsonLayer(multiLineStringGeojson, 'line', 'multiLineString');
+                stormTracksLayerArr.push('multiLineString');
+                // add the circles on the line edges
+                mapFuncs.setGeojsonLayer(linePointGeojson, 'lineCircleEdge', 'linePoint');
+                stormTracksLayerArr.push('linePoint');
+                // add the red points for isolated cells
+                mapFuncs.setGeojsonLayer(singlePointGeojson, 'circle', 'singlePoint');
+                stormTracksLayerArr.push('singlePoint');
+                // add the blue points for the start of a storm track
+                mapFuncs.setGeojsonLayer(mainLinePointGeojson, 'lineCircle', 'mainLinePoint');
+                stormTracksLayerArr.push('mainLinePoint');
 
-        // add the storm track lines
-        mapFuncs.setGeojsonLayer(multiLineStringGeojson, 'line', 'multiLineString');
-        stormTracksLayerArr.push('multiLineString');
-        // add the circles on the line edges
-        mapFuncs.setGeojsonLayer(linePointGeojson, 'lineCircleEdge', 'linePoint');
-        stormTracksLayerArr.push('linePoint');
-        // add the red points for isolated cells
-        mapFuncs.setGeojsonLayer(singlePointGeojson, 'circle', 'singlePoint');
-        stormTracksLayerArr.push('singlePoint');
-        // add the blue points for the start of a storm track
-        mapFuncs.setGeojsonLayer(mainLinePointGeojson, 'lineCircle', 'mainLinePoint');
-        stormTracksLayerArr.push('mainLinePoint');
-
-        document.getElementById('allStormTracksLayers').innerHTML = JSON.stringify(stormTracksLayerArr);
-        var stLayersText = document.getElementById('allStormTracksLayers').innerHTML;
-        var stLayers = stLayersText.replace(/"/g, '').replace(/\[/g, '').replace(/\]/g, '').split(',');
-        // setting layer orders
-        for (key in stLayers) {
-            mapFuncs.moveMapLayer(stLayers[key])
-        }
-    });
+                document.getElementById('allStormTracksLayers').innerHTML = JSON.stringify(stormTracksLayerArr);
+                var stLayersText = document.getElementById('allStormTracksLayers').innerHTML;
+                var stLayers = stLayersText.replace(/"/g, '').replace(/\[/g, '').replace(/\]/g, '').split(',');
+                // setting layer orders
+                for (key in stLayers) {
+                    mapFuncs.moveMapLayer(stLayers[key])
+                }
+            });
+        });
+    }
 
     map.on('click', 'mainLinePoint', (e) => { cellClick(e) });
     map.on('click', 'singlePoint', (e) => { cellClick(e) });
