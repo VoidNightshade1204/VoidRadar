265baed6b35d1959b5848858c88764476a55a485
SteepAtticStairs <lawlorcube06@gmail.com>
2022-09-26 14:42:45 -0400

    added some files for stackoverflow

--- app/satellite/processData/fromNC/index.js
+++ app/satellite/processData/fromNC/index.js
@@ -3,6 +3,7 @@ const { exec } = require("child_process");
 
 /*
 https://gist.github.com/spestana/876fcd2ca1fbac441f28243bdc406c22
+https://edc.occ-data.org/goes16/getdata/ (this gives product abbreviations)
 */
 // gdal_translate -ot float32 -unscale -CO COMPRESS=deflate NETCDF:"goes16-c13.nc":Rad ugly.tif
 // gdal_translate -ot Byte -of png -scale 0 650 0 255 ugly.tif bw.png
@@ -34,7 +35,7 @@ function execCmd(command, cb) {
 var args = process.argv;
 
 if (args.includes('changeColor')) {
-    execCmd(`gdaldem color-relief -alpha bwProj.png color.txt colored.png`, function(){});
+    execCmd(`gdaldem color-relief -alpha bwProj.png IR4AVHRR6.cpt colored.png`, function(){});
 } else {
     removeFile('bw.png');
     removeFile('bw.png.aux.xml');
@@ -44,9 +45,14 @@ if (args.includes('changeColor')) {
     removeFile('colored.png.aux.xml');
     removeFile('ugly.tif');
 
-    execCmd(`gdal_translate -ot float32 -unscale -CO COMPRESS=deflate NETCDF:"goes16-c13.nc":Rad ugly.tif`, function() {
-    execCmd(`gdal_translate -ot Byte -of png -scale 0 650 0 255 ugly.tif bw.png`, function() {
+    execCmd(`gdal_translate -unscale NETCDF:"goes16-c13.nc":Rad ugly.tif`, function() {
+    execCmd(`gdal_translate -ot Byte -of png ugly.tif bw.png`, function() {
     execCmd(`gdalwarp -t_srs EPSG:4326 -dstnodata -999.0 bw.png bwProj.png`, function() {
-    execCmd(`gdaldem color-relief -alpha bwProj.png color.txt colored.png`, function(){});
+    execCmd(`gdaldem color-relief -alpha bwProj.png IR4AVHRR6.cpt colored.png`, function(){});
+
+    //execCmd(`gdal_translate -ot float32 -unscale -CO COMPRESS=deflate NETCDF:"goes16-c13.nc":Rad ugly.tif`, function() {
+    //execCmd(`gdal_translate -ot Byte -of png -scale 0 650 0 255 ugly.tif bw.png`, function() {
+    //execCmd(`gdalwarp -t_srs EPSG:4326 -dstnodata -999.0 bw.png bwProj.png`, function() {
+    //execCmd(`gdaldem color-relief -alpha bwProj.png color.txt colored.png`, function(){});
     });});});
 }
