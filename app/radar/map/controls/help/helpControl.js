8de1d63f4ad44ddfed18334df0104243e44ffd3e
SteepAtticStairs <lawlorcube06@gmail.com>
2022-10-03 19:35:04 -0400

    updated config.yml

--- app/radar/map/controls/help/helpControl.js
+++ app/radar/map/controls/help/helpControl.js
@@ -1,25 +1,6 @@
 const ut = require('../../../utils');
 const createOffCanvasItem = require('../../../menu/createOffCanvasItem');
 
-const helpContent = 
-`This is a help screen coming soon.
-
-<!-- This is a <b>help</b> <u>screen</u>
-<br>
-<br>
-Please <strike>balls</strike>s.
-
-<br>
-https://stackoverflow.com/a/34202666/18758797
-<div style="display: flex; justify-content: center;">
-    <img src="../../../../../logo.png" width="20%" height="auto">
-</div>
-<br>
-
-More <b>content</b> <u>for</u> testing.
-<br>
-Lm <strike>a</strike>o. -->`
-
 //$.get('app/radar/map/controls/help/helpControlContent.html', function(data) {
     createOffCanvasItem({
         'id': 'helpMenuItem',
@@ -31,7 +12,7 @@ Lm <strike>a</strike>o. -->`
         ut.spawnModal({
             'title': 'Help',
             'headerColor': 'alert-info',
-            'body': helpContent
+            'body': data
         })
     })
 //})
