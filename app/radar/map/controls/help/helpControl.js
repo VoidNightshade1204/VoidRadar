bec0c2b490527712b0dc1f053ec5f391f80d0b73
SteepAtticStairs <lawlorcube06@gmail.com>
2022-10-03 19:29:46 -0400

    help screen content is in the same js file

--- app/radar/map/controls/help/helpControl.js
+++ app/radar/map/controls/help/helpControl.js
@@ -1,7 +1,26 @@
 const ut = require('../../../utils');
 const createOffCanvasItem = require('../../../menu/createOffCanvasItem');
 
-$.get('app/radar/map/controls/help/helpControlContent.html', function(data) {
+const helpContent = 
+`This is a help screen coming soon.
+
+<!-- This is a <b>help</b> <u>screen</u>
+<br>
+<br>
+Please <strike>balls</strike>s.
+
+<br>
+https://stackoverflow.com/a/34202666/18758797
+<div style="display: flex; justify-content: center;">
+    <img src="../../../../../logo.png" width="20%" height="auto">
+</div>
+<br>
+
+More <b>content</b> <u>for</u> testing.
+<br>
+Lm <strike>a</strike>o. -->`
+
+//$.get('app/radar/map/controls/help/helpControlContent.html', function(data) {
     createOffCanvasItem({
         'id': 'helpMenuItem',
         'class': 'alert alert-secondary offCanvasMenuItem',
@@ -12,7 +31,7 @@ $.get('app/radar/map/controls/help/helpControlContent.html', function(data) {
         ut.spawnModal({
             'title': 'Help',
             'headerColor': 'alert-info',
-            'body': data
+            'body': helpContent
         })
     })
-})
+//})
