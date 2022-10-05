94faca2eb85cca6db33361ba1f3378f8022c99b8
SteepAtticStairs <lawlorcube06@gmail.com>
2022-10-05 15:25:10 -0400

    added setinterval for the time diff display

--- app/radar/dom/l3info.js
+++ app/radar/dom/l3info.js
@@ -22,21 +22,29 @@ function showL3Info(l3rad) {// //showPlotBtn();
 
         document.getElementById('radarTime').innerHTML = `&nbsp;&nbsp;${finalRadarDateTime}`;
 
-        const dateDiff = ut.getDateDiff(fileDateObj, new Date());
-        var formattedDateDiff;
-        if (dateDiff.s) { formattedDateDiff = `${dateDiff.s}s`; }
-        if (dateDiff.m) { formattedDateDiff = `${dateDiff.m}m ${dateDiff.s}s`; }
-        if (dateDiff.h) { formattedDateDiff = `${dateDiff.h}h ${dateDiff.m}m`; }
-        if (dateDiff.d) { formattedDateDiff = `${dateDiff.d}d ${dateDiff.h}h`; }
+        function showTimeDiff() {
+            const dateDiff = ut.getDateDiff(fileDateObj, new Date());
+            var formattedDateDiff;
+            if (dateDiff.s) { formattedDateDiff = `${dateDiff.s}s`; }
+            if (dateDiff.m) { formattedDateDiff = `${dateDiff.m}m ${dateDiff.s}s`; }
+            if (dateDiff.h) { formattedDateDiff = `${dateDiff.h}h ${dateDiff.m}m`; }
+            if (dateDiff.d) { formattedDateDiff = `${dateDiff.d}d ${dateDiff.h}h`; }
 
-        $('#top-right').removeClass();
-        // greater than 1 hour or 1 day OR greater than or equal to 0 hours 30 minutes
-        if (dateDiff.h > 0 || dateDiff.d > 0 || (dateDiff.h == 0 && dateDiff.m >= 30)) { $('#top-right').addClass('old-file'); }
-        // greater than or equal to 0 hours 10 minutes
-        if (dateDiff.h == 0 && dateDiff.m >= 10) { $('#top-right').addClass('recent-file'); }
-        // less than 0 hours 10 minutes
-        if (dateDiff.h == 0 && dateDiff.m < 10) { $('#top-right').addClass('new-file'); }
-        document.getElementById('top-right').innerHTML = formattedDateDiff;
+            $('#top-right').removeClass();
+            // greater than 1 hour or 1 day OR greater than or equal to 0 hours 30 minutes
+            if (dateDiff.h > 0 || dateDiff.d > 0 || (dateDiff.h == 0 && dateDiff.m >= 30)) { $('#top-right').addClass('old-file'); }
+            // greater than or equal to 0 hours 10 minutes
+            if (dateDiff.h == 0 && dateDiff.m >= 10) { $('#top-right').addClass('recent-file'); }
+            // less than 0 hours 10 minutes
+            if (dateDiff.h == 0 && dateDiff.m < 10) { $('#top-right').addClass('new-file'); }
+            document.getElementById('top-right').innerHTML = formattedDateDiff;
+        }
+        if (window.countInterval) {
+            clearInterval(window.countInterval)
+        }
+        showTimeDiff();
+        // update the time difference every 10 seconds
+        window.countInterval = setInterval(showTimeDiff, 10000);
 
         if ($('#dataDiv').data('fromFileUpload')) {
             // shrink the map header because the file upload box is no longer there
