9df6d87dc75ce34ff470a3d081e8db0fa0e19cde
SteepAtticStairs <lawlorcube06@gmail.com>
2022-10-04 20:55:37 -0400

    updated file recency logic

--- app/radar/dom/l3info.js
+++ app/radar/dom/l3info.js
@@ -43,9 +43,12 @@ function showL3Info(l3rad) {// //showPlotBtn();
         if (dateDiff.d) { formattedDateDiff = `${dateDiff.d}d ${dateDiff.h}h`; }
 
         $('#top-right').removeClass();
-        if (dateDiff.m >= 30) { $('#top-right').addClass('old-file'); }
-        if (dateDiff.m >= 10) { $('#top-right').addClass('recent-file'); }
-        if (dateDiff.m < 10) { $('#top-right').addClass('new-file'); }
+        // greater than 1 hour or 1 day OR greater than or equal to 0 hours 30 minutes
+        if (dateDiff.h > 0 || dateDiff.d > 0 || (dateDiff.h == 0 && dateDiff.m >= 30)) { $('#top-right').addClass('old-file'); }
+        // greater than or equal to 0 hours 10 minutes
+        if (dateDiff.h == 0 && dateDiff.m >= 10) { $('#top-right').addClass('recent-file'); }
+        // less than 0 hours 10 minutes
+        if (dateDiff.h == 0 && dateDiff.m < 10) { $('#top-right').addClass('new-file'); }
         document.getElementById('top-right').innerHTML = formattedDateDiff;
 
         if ($('#dataDiv').data('fromFileUpload')) {
