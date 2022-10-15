c48e45e6b1dcd27974d6b2a857c08f02a6152571
SteepAtticStairs <lawlorcube06@gmail.com>
2022-10-15 17:52:57 -0400

    added color to the search items

--- app/hurricanes/historical/eventListeners.js
+++ app/hurricanes/historical/eventListeners.js
@@ -1,4 +1,24 @@
 const parseHurricaneFile = require('./plotIBTRACS');
+const ut = require('../../radar/utils');
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+function catInformation(mode, cat) {
+    cat = parseInt(cat);
+    if (mode == 'color') {
+        var rgb = hexToRgb(ut.sshwsValues[cat + 1][1])
+        return `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 0.5)`;
+    } else if (mode == 'name') {
+        return ut.sshwsValues[cat + 1][2];
+    }
+}
 
 function initHurricaneArchiveListeners() {
     // <li><a class="dropdown-item" href="#" value="al">Atlantic</a></li>
@@ -42,14 +62,15 @@ function initHurricaneArchiveListeners() {
                 var name = stormList[n][1];
                 var year = stormList[n][2];
                 var basin = stormList[n][3];
+                var maxCategory = stormList[n][4];
                 name = capitalizeFirstLetter(name.toLowerCase());
                 if (name.toLowerCase().includes(textValue)) {
-                    buildDropdownElement(`${name} ${year}`, sid)
+                    buildDropdownElement(`${name} ${year} ${catInformation('name', maxCategory)}`, sid, catInformation('color', maxCategory))
                 } else if (year.includes(textValue)) {
-                    buildDropdownElement(`${name} ${year}`, sid)
+                    buildDropdownElement(`${name} ${year} ${catInformation('name', maxCategory)}`, sid, catInformation('color', maxCategory))
                 }
             } catch (e) {
-                console.warn(e);
+                //console.warn(e);
             }
         }
         var lengthOfStormSuggestions = document.getElementById("haStormNameDropdownMenu").getElementsByTagName("li").length;
