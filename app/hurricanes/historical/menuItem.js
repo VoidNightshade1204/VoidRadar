56e3bbba60f456ebd547da2c495d95dc1ac4b86f
SteepAtticStairs <lawlorcube06@gmail.com>
2022-10-19 19:18:50 -0400

    added minimizer button to the hurricane archive map control

--- app/hurricanes/historical/menuItem.js
+++ app/hurricanes/historical/menuItem.js
@@ -43,6 +43,81 @@ $('#haClearMap').on('click', function() {
     }
 })
 
+$('#dataDiv').data('isHaControlMinimized', false);
+
+// https://stackoverflow.com/a/15191130/18758797
+$.fn.animateRotate = function (angle, duration, easing, complete) {
+    var args = $.speed(duration, easing, complete);
+    var step = args.step;
+    return this.each(function (i, e) {
+        args.complete = $.proxy(args.complete, e);
+        args.step = function (now) {
+            $.style(e, 'transform', 'rotate(' + now + 'deg)');
+            if (step) return step.apply(e, arguments);
+        };
+
+        $({ deg: 0 }).animate({ deg: angle }, args);
+    });
+};
+
+function flipIcon(minimizeOrMaximize) {
+    function rotateThing(deg) {
+        $('#haMapControlMinimizeIcon').animateRotate(deg, {
+            duration: 250,
+            easing: 'linear',
+            complete: function () {},
+            step: function () {}
+        });
+    }
+    if (minimizeOrMaximize == 'minimize') {
+        $('#haMapControlMinimizeIcon').removeClass('fa-chevron-right');
+        $('#haMapControlMinimizeIcon').addClass('fa-chevron-left');
+        rotateThing(180);
+    } else if (minimizeOrMaximize == 'maximize') {
+        $('#haMapControlMinimizeIcon').removeClass('fa-chevron-left');
+        $('#haMapControlMinimizeIcon').addClass('fa-chevron-right');
+        rotateThing(-180);
+    }
+}
+
+$('#haMapControlMinimize').on('click', function() {
+    var buffer = 4;
+    //var widthToSubtract = $('#hurricaneArchiveMapControl').width() - $('#haMapControlMinimize').width();
+    if (!$('#dataDiv').data('isHaControlMinimized')) {
+        $('#dataDiv').data('isHaControlMinimized', true);
+
+        $('#haClearMapOuter').css('max-height', $('#haClearMapOuter').height());
+        $('#haMapControlText').css('max-height', $('#haMapControlText').height());
+        $('.haClearBtnContents').css('visibility', 'hidden');
+        $('#hurricaneArchiveMapControl').css('overflow', 'hidden');
+
+        flipIcon('minimize');
+
+        $('#hurricaneArchiveMapControl').animate({
+            width: $('#haMapControlMinimize').width() + buffer,
+        }, 250)
+    } else if ($('#dataDiv').data('isHaControlMinimized')) {
+        $('#dataDiv').data('isHaControlMinimized', false);
+        $('#haClearMapOuter').css('visibility', 'visible');
+        $('.haClearBtnContents').css('visibility', 'hidden');
+
+        flipIcon('maximize');
+
+        var el = $('#hurricaneArchiveMapControl'),
+            curWidth = el.width(),
+            autoWidth = el.css('width', 'fit-content').width();
+        el.width(curWidth).animate({
+            width: autoWidth + buffer
+        }, 250, function() {
+            el.css('width', 'fit-content');
+            $('#haClearMapOuter').css('max-height', '');
+            $('#haMapControlText').css('max-height', '');
+            $('.haClearBtnContents').css('visibility', 'visible');
+            $('#hurricaneArchiveMapControl').css('overflow', 'scroll');
+        });
+    }
+})
+
 function shouldStartRightAway() {
     if (!map.loaded()) {
         map.on('load', function() {
@@ -52,4 +127,4 @@ function shouldStartRightAway() {
         startRightAway();
     }
 }
-//shouldStartRightAway();
+shouldStartRightAway();
