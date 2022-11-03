45715915ebb4db8ce8222acd9ede18cd1b45c7a8
SteepAtticStairs <lawlorcube06@gmail.com>
2022-11-02 22:14:19 -0400

    update hurricane css classes

--- app/hurricanes/createCssClasses.js
+++ app/hurricanes/createCssClasses.js
@@ -1,50 +1,92 @@
 const ut = require('../radar/utils');
 
+// https://github.com/mapbox/mapbox-gl-js/issues/2848#issuecomment-354447882
+
 function createCssClasses() {
     $("<style>")
     .prop("type", "text/css")
     .html(`
-    .TD .mapboxgl-popup-content { background-color: ${ut.sshwsValues[0][1]}; color: black; }
-    .TS .mapboxgl-popup-content { background-color: ${ut.sshwsValues[1][1]}; color: black; }
-    .C1 .mapboxgl-popup-content { background-color: ${ut.sshwsValues[2][1]}; color: black; }
-    .C2 .mapboxgl-popup-content { background-color: ${ut.sshwsValues[3][1]}; color: black; }
-    .C3 .mapboxgl-popup-content { background-color: ${ut.sshwsValues[4][1]}; color: white; }
-    .C4 .mapboxgl-popup-content { background-color: ${ut.sshwsValues[5][1]}; color: white; }
-    .C5 .mapboxgl-popup-content { background-color: ${ut.sshwsValues[6][1]}; color: white; }
-    .Other .mapboxgl-popup-content { background-color: ${ut.sshwsValues[7][1]}; color: white; }
+    .TD.mapboxgl-popup-anchor-bottom .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[0][1]}; }
+    .TD.mapboxgl-popup-anchor-top .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[0][1]}; }
+    .TD.mapboxgl-popup-anchor-left .mapboxgl-popup-tip { border-right-color: ${ut.sshwsValues[0][1]}; }
+    .TD.mapboxgl-popup-anchor-right .mapboxgl-popup-tip { border-left-color: ${ut.sshwsValues[0][1]}; }
+    .TD.mapboxgl-popup-anchor-top-left .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[0][1]}; }
+    .TD.mapboxgl-popup-anchor-top-right .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[0][1]}; }
+    .TD.mapboxgl-popup-anchor-bottom-left .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[0][1]}; }
+    .TD.mapboxgl-popup-anchor-bottom-right .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[0][1]}; }
 
-    .TD .mapboxgl-popup-tip { 
-        border-top-color: ${ut.sshwsValues[0][1]};
-        border-bottom-color: ${ut.sshwsValues[0][1]};
-    }
-    .TS .mapboxgl-popup-tip {
-        border-top-color: ${ut.sshwsValues[1][1]};
-        border-bottom-color: ${ut.sshwsValues[1][1]};
-    }
-    .C1 .mapboxgl-popup-tip {
-        border-top-color: ${ut.sshwsValues[2][1]};
-        border-bottom-color: ${ut.sshwsValues[2][1]};
-    }
-    .C2 .mapboxgl-popup-tip {
-        border-top-color: ${ut.sshwsValues[3][1]};
-        border-bottom-color: ${ut.sshwsValues[3][1]};
-    }
-    .C3 .mapboxgl-popup-tip {
-        border-top-color: ${ut.sshwsValues[4][1]};
-        border-bottom-color: ${ut.sshwsValues[4][1]};
-    }
-    .C4 .mapboxgl-popup-tip {
-        border-top-color: ${ut.sshwsValues[5][1]};
-        border-bottom-color: ${ut.sshwsValues[5][1]};
-    }
-    .C5 .mapboxgl-popup-tip {
-        border-top-color: ${ut.sshwsValues[6][1]};
-        border-bottom-color: ${ut.sshwsValues[6][1]};
-    }
-    .Other .mapboxgl-popup-tip {
-        border-top-color: ${ut.sshwsValues[7][1]};
-        border-bottom-color: ${ut.sshwsValues[7][1]};
-    }`)
+    .TS.mapboxgl-popup-anchor-bottom .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[1][1]}; }
+    .TS.mapboxgl-popup-anchor-top .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[1][1]}; }
+    .TS.mapboxgl-popup-anchor-left .mapboxgl-popup-tip { border-right-color: ${ut.sshwsValues[1][1]}; }
+    .TS.mapboxgl-popup-anchor-right .mapboxgl-popup-tip { border-left-color: ${ut.sshwsValues[1][1]}; }
+    .TS.mapboxgl-popup-anchor-top-left .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[1][1]}; }
+    .TS.mapboxgl-popup-anchor-top-right .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[1][1]}; }
+    .TS.mapboxgl-popup-anchor-bottom-left .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[1][1]}; }
+    .TS.mapboxgl-popup-anchor-bottom-right .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[1][1]}; }
+
+    .C1.mapboxgl-popup-anchor-bottom .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[2][1]}; }
+    .C1.mapboxgl-popup-anchor-top .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[2][1]}; }
+    .C1.mapboxgl-popup-anchor-left .mapboxgl-popup-tip { border-right-color: ${ut.sshwsValues[2][1]}; }
+    .C1.mapboxgl-popup-anchor-right .mapboxgl-popup-tip { border-left-color: ${ut.sshwsValues[2][1]}; }
+    .C1.mapboxgl-popup-anchor-top-left .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[2][1]}; }
+    .C1.mapboxgl-popup-anchor-top-right .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[2][1]}; }
+    .C1.mapboxgl-popup-anchor-bottom-left .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[2][1]}; }
+    .C1.mapboxgl-popup-anchor-bottom-right .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[2][1]}; }
+
+    .C2.mapboxgl-popup-anchor-bottom .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[3][1]}; }
+    .C2.mapboxgl-popup-anchor-top .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[3][1]}; }
+    .C2.mapboxgl-popup-anchor-left .mapboxgl-popup-tip { border-right-color: ${ut.sshwsValues[3][1]}; }
+    .C2.mapboxgl-popup-anchor-right .mapboxgl-popup-tip { border-left-color: ${ut.sshwsValues[3][1]}; }
+    .C2.mapboxgl-popup-anchor-top-left .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[3][1]}; }
+    .C2.mapboxgl-popup-anchor-top-right .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[3][1]}; }
+    .C2.mapboxgl-popup-anchor-bottom-left .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[3][1]}; }
+    .C2.mapboxgl-popup-anchor-bottom-right .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[3][1]}; }
+
+    .C3.mapboxgl-popup-anchor-bottom .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[4][1]}; }
+    .C3.mapboxgl-popup-anchor-top .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[4][1]}; }
+    .C3.mapboxgl-popup-anchor-left .mapboxgl-popup-tip { border-right-color: ${ut.sshwsValues[4][1]}; }
+    .C3.mapboxgl-popup-anchor-right .mapboxgl-popup-tip { border-left-color: ${ut.sshwsValues[4][1]}; }
+    .C3.mapboxgl-popup-anchor-top-left .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[4][1]}; }
+    .C3.mapboxgl-popup-anchor-top-right .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[4][1]}; }
+    .C3.mapboxgl-popup-anchor-bottom-left .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[4][1]}; }
+    .C3.mapboxgl-popup-anchor-bottom-right .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[4][1]}; }
+
+    .C4.mapboxgl-popup-anchor-bottom .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[5][1]}; }
+    .C4.mapboxgl-popup-anchor-top .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[5][1]}; }
+    .C4.mapboxgl-popup-anchor-left .mapboxgl-popup-tip { border-right-color: ${ut.sshwsValues[5][1]}; }
+    .C4.mapboxgl-popup-anchor-right .mapboxgl-popup-tip { border-left-color: ${ut.sshwsValues[5][1]}; }
+    .C4.mapboxgl-popup-anchor-top-left .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[5][1]}; }
+    .C4.mapboxgl-popup-anchor-top-right .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[5][1]}; }
+    .C4.mapboxgl-popup-anchor-bottom-left .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[5][1]}; }
+    .C4.mapboxgl-popup-anchor-bottom-right .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[5][1]}; }
+
+    .C5.mapboxgl-popup-anchor-bottom .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[6][1]}; }
+    .C5.mapboxgl-popup-anchor-top .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[6][1]}; }
+    .C5.mapboxgl-popup-anchor-left .mapboxgl-popup-tip { border-right-color: ${ut.sshwsValues[6][1]}; }
+    .C5.mapboxgl-popup-anchor-right .mapboxgl-popup-tip { border-left-color: ${ut.sshwsValues[6][1]}; }
+    .C5.mapboxgl-popup-anchor-top-left .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[6][1]}; }
+    .C5.mapboxgl-popup-anchor-top-right .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[6][1]}; }
+    .C5.mapboxgl-popup-anchor-bottom-left .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[6][1]}; }
+    .C5.mapboxgl-popup-anchor-bottom-right .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[6][1]}; }
+
+    .Other.mapboxgl-popup-anchor-bottom .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[7][1]}; }
+    .Other.mapboxgl-popup-anchor-top .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[7][1]}; }
+    .Other.mapboxgl-popup-anchor-left .mapboxgl-popup-tip { border-right-color: ${ut.sshwsValues[7][1]}; }
+    .Other.mapboxgl-popup-anchor-right .mapboxgl-popup-tip { border-left-color: ${ut.sshwsValues[7][1]}; }
+    .Other.mapboxgl-popup-anchor-top-left .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[7][1]}; }
+    .Other.mapboxgl-popup-anchor-top-right .mapboxgl-popup-tip { border-bottom-color: ${ut.sshwsValues[7][1]}; }
+    .Other.mapboxgl-popup-anchor-bottom-left .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[7][1]}; }
+    .Other.mapboxgl-popup-anchor-bottom-right .mapboxgl-popup-tip { border-top-color: ${ut.sshwsValues[7][1]}; }
+
+    .TD .mapboxgl-popup-content { background-color: ${ut.sshwsValues[0][1]}; color: black; border-radius: 10px; }
+    .TS .mapboxgl-popup-content { background-color: ${ut.sshwsValues[1][1]}; color: black; border-radius: 10px; }
+    .C1 .mapboxgl-popup-content { background-color: ${ut.sshwsValues[2][1]}; color: black; border-radius: 10px; }
+    .C2 .mapboxgl-popup-content { background-color: ${ut.sshwsValues[3][1]}; color: black; border-radius: 10px; }
+    .C3 .mapboxgl-popup-content { background-color: ${ut.sshwsValues[4][1]}; color: white; border-radius: 10px; }
+    .C4 .mapboxgl-popup-content { background-color: ${ut.sshwsValues[5][1]}; color: white; border-radius: 10px; }
+    .C5 .mapboxgl-popup-content { background-color: ${ut.sshwsValues[6][1]}; color: white; border-radius: 10px; }
+    .Other .mapboxgl-popup-content { background-color: ${ut.sshwsValues[7][1]}; color: white; border-radius: 10px; }
+    `)
     .appendTo("head");
 }
 
