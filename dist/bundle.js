9f2bc86191507e0762e1f8f2ded27969be089385
SteepAtticStairs <lawlorcube06@gmail.com>
2022-11-02 22:05:37 -0400

    no more duplicate alerts (check WMO identifier)

--- dist/bundle.js
+++ dist/bundle.js
@@ -325,12 +325,16 @@ function rgbToRGBA(rgb, opacity) {
 function addMarker(e) {
     var popupItem = '';
     var alertContentObj = {};
+    var alreadyAddedWmoIDs = [];
     for (key in e.features) {
         var id = `${Date.now()}alert`;
         var properties = e.features[key].properties;
         var parameters = JSON.parse(properties.parameters);
         //console.log(e.features[key])
         console.log(properties)
+        var wmoId = parameters.WMOidentifier[0];
+        if (!alreadyAddedWmoIDs.includes(wmoId)) {
+        alreadyAddedWmoIDs.push(wmoId);
 
         var initColor = getPolygonColors(properties.event);
         var backgroundColor = initColor;
@@ -419,6 +423,7 @@ function addMarker(e) {
 
         //popupItem += '<br>';
     }
+    }
     const popup = new mapboxgl.Popup({ className: 'alertPopup', maxWidth: '1000' })
         .setLngLat(e.lngLat)
         .setHTML(popupItem)
@@ -440,152 +445,6 @@ function addMarker(e) {
             'textColor': alertContentObj[id].textColor,
         })
     })
-    // for (key in e.features) {
-    //     var properties = e.features[key].properties;
-    //     var parameters = JSON.parse(properties.parameters);
-    //     console.log(parameters)
-
-    //     var alertURL = properties['@id'];
-
-    //     var alertModalBody = 
-    //         `<b>${properties.event}
-    //         <br>${properties.senderName}
-    //         <br>${properties.headline}</b>
-    //         <br><u>${ut.printFancyTime(new Date(properties.sent))}</u>
-    //         <br><u>${parameters.WMOidentifier}</u>
-    //         <br><u>${parameters.VTEC}</u>
-    //         <br><u>${properties.areaDesc}</u>
-    //         <br>${parameters.NWSheadline}
-    //         <br>${properties.description}
-    //         <br>${properties.instruction}`
-    //     //document.getElementById('alertModalBody').innerHTML = alertModalBody;
-    //     //var alertTitle = `Alert #${parseInt(key) + 1}`;
-    //     var color = getPolygonColors(properties.event);
-    //     var alertTitle = 
-    //         `<div style="color: white"><b style="color: ${color};">${properties.event}</b>`;
-
-    //     var lineSpace = '';
-    //     var lineBreak = `<br>`;
-    //     var amountOfParams = 0;
-    //     if (parameters.hasOwnProperty('maxHailSize')) {
-    //         if (amountOfParams == 0) { alertTitle += lineBreak; }
-    //         if (lineSpace == '' && amountOfParams != 0) { lineSpace = '&nbsp;&nbsp;&nbsp;'; }
-    //         alertTitle += `${lineSpace}<b>Hail:</b> ${parameters.maxHailSize}`;
-    //         amountOfParams++;
-    //     }
-    //     if (parameters.hasOwnProperty('maxWindGust')) {
-    //         if (amountOfParams == 0) { alertTitle += lineBreak; }
-    //         if (lineSpace == '' && amountOfParams != 0) { lineSpace = '&nbsp;&nbsp;&nbsp;'; }
-    //         alertTitle += `${lineSpace}<b>Wind:</b> ${parameters.maxWindGust}</div>`;
-    //         amountOfParams++;
-    //     }
-    //     alertTitle += '</div>'
-
-    //     const popup = new mapboxgl.Popup({ className: 'alertPopup' })
-    //         .setLngLat(e.lngLat)
-    //         .setHTML(alertTitle)
-    //         .addTo(map);
-    //     //var color = getPolygonColors(properties.event);
-    //     //updateAccordion(key, alertTitle, false, alertModalBody, rgbToRGBA(color, 0.5));
-    // }
-
-    // open the modal
-    //$('#alertModalTrigger').click();
-
-
-
-    // var pointURL = `https://api.weather.gov/alerts?active=1&point=${e.lngLat.lat},${e.lngLat.lng}`
-    // console.log(pointURL)
-    // var alertPopupContent = `<div><b>No Alerts</b></div>`
-    // $.getJSON(pointURL, function(data) {
-    //     if (data.features.length != 0) {
-    //         alertPopupContent = '<div id="mainPopupDiv" style="overflow-y: scroll; max-height: 150px;">';
-    //         for (var n = 0; n < data.features.length; n++) {
-    //             var properties = data.features[n].properties
-
-    //             var alertEvent = properties.event;
-    //             var alertSenderName = properties.senderName;
-    //             var alertHeadline = properties.headline;
-    //             var alertDescription = properties.description;
-    //             var alertInstruction = properties.instruction;
-
-    //             var alertID = properties.id;
-    //             var alertWMOidenifier = properties.parameters.WMOidentifier[0];
-    //             var alertVTEC = "No VTEC for this alert."
-    //             var alertNWSheadline = "No headline for this alert."
-
-    //             if (alertInstruction == null) {
-    //                 alertInstruction = "No instruction for this alert."
-    //             }
-
-    //             if (properties.parameters.hasOwnProperty('NWSheadline')) {
-    //                 alertNWSheadline = properties.parameters.NWSheadline;
-    //             }
-    //             if (properties.parameters.hasOwnProperty('VTEC')) {
-    //                 alertVTEC = properties.parameters.VTEC[0];
-    //             }
-
-    //             var alertSeverity = properties.severity;
-    //             var alertUrgency = properties.urgency;
-
-    //             var alertSentTime = new Date(properties.sent);
-    //             var alertEffectiveTime = new Date(properties.effective);
-    //             var alertOnsetTime = new Date(properties.onset);
-    //             var alertExpiresTime = new Date(properties.expires);
-    //             var alertEndsTime = new Date(properties.ends);
-    //             alertPopupContent += `
-    //             <div class='ughh' id='headlineExpander${n}' style='cursor: pointer'>
-    //                 <b>${alertEvent}<br>${alertSenderName}</b>
-    //             </div>
-    //             <div id='alertBody${n}' style='display: none'>
-    //                 <br><b>Send Time: </b>${ut.printFancyTime(alertSentTime)}
-    //                 <br><b>Effective Time: </b>${ut.printFancyTime(alertEffectiveTime)}
-    //                 <br><b>Onset Time: </b>${ut.printFancyTime(alertOnsetTime)}
-    //                 <br><b>Expires Time: </b>${ut.printFancyTime(alertExpiresTime)}
-    //                 <br><b>Ends Time: </b>${ut.printFancyTime(alertEndsTime)}
-    //                 <br><br><b><i>${alertSeverity}</i></b>, <b><i>${alertUrgency}</i></b><br><br>
-    //                 <b>WMO Identifier:</b> ${alertWMOidenifier}<br>
-    //                 <b>VTEC:</b> ${alertVTEC}<br>
-    //                 <b>NWS Headline:</b> ${alertNWSheadline}<br>
-    //                 <b>ID:</b><div id='alertNameDiv'>${alertID}</div><br><br>
-    //                 <b>Headline:</b> ${alertHeadline}<br>
-    //                 <b>Description:</b> ${alertDescription}<br>
-    //                 <b>Instructions:</b> ${alertInstruction}
-    //             </div>`;
-    //             if (n != data.features.length - 1) {
-    //                 alertPopupContent += '<br>'
-    //             }
-    //         }
-    //         alertPopupContent += '</div>'
-    //     }
-
-    //     var thepop = new mapboxgl.Popup()
-    //         .setLngLat([e.lngLat.lng, e.lngLat.lat])
-    //         .setHTML(alertPopupContent)
-    //         .addTo(map);
-    //     document.getElementById('mainPopupDiv').style.height = 'auto';
-
-    //     function toggleDisplay(theelem) {
-    //         if (document.getElementById(theelem).style.display == 'none') {
-    //             document.getElementById(theelem).style.display = 'block'
-    //             document.getElementById('mainPopupDiv').style.height = '150px';
-    //         } else if (document.getElementById(theelem).style.display == 'block') {
-    //             document.getElementById(theelem).style.display = 'none'
-    //             document.getElementById('mainPopupDiv').style.height = 'auto';
-    //         }
-    //     }
-
-    //     function addAlertListner(numbe) {
-    //         document.getElementById('headlineExpander' + numbe).addEventListener('click', function() {
-    //             toggleDisplay('alertBody' + numbe)
-    //         })
-    //     }
-
-    //     var allHeadlines = document.getElementsByClassName('ughh')
-    //     for (var f = 0; f < allHeadlines.length; f++) {
-    //         addAlertListner(f)
-    //     }
-    // })
 }
 
 module.exports = addMarker;
